b7321acf6587e5958385a09dc76617708b9bc858
Nanjou <lowie375@gmail.com>
2024-04-04 21:12:02 -0700

    Add init helpers + core state display code
    Co-authored-by: natester <natester@github.students.cs.ubc.ca>
    Co-authored-by: tglenn01 <tglenn01@github.students.cs.ubc.ca>
    Co-authored-by: nxengilo <nxengilo@github.students.cs.ubc.ca>

---
+++ hanabi.pl
@@ -0,0 +1,101 @@
+% welcome to HANABI - fireworks game where you feel dumb af
+
+% In the before times...
+% Before there was VSCode LiveShare or Discord
+% We interacted in REAL LIFE!
+% (ooh, scary)
+
+% Long ago, the four nations lived together in harmony
+% Then, everything changed when the Sudoku Nation attacked
+% Only the Avatar, master of all four elements, could stop them
+% But when the world needed him most, he couldn't solve a puzzle
+
+/*
+variables storing data in a recursive relation
+- state: Played, Discarded, Deck, Tokens, Misses, Hands
+
+card(Col, Num, (ColKnown, NumKnown))
+- Col = red ; yellow ; green ; blue ; white .
+
+*/
+
+[main].
+
+% source https://stackoverflow.com/questions/32691313/counter-in-prolog
+
+:- dynamic played_cards/1.
+:- dynamic information_token/1.
+:- dynamic miss_chances/1.
+
+init_played_cards :-
+    retractall(played_cards(_)),
+    assertz(played_cards(card(red, 0, (_, _)))),
+    assertz(played_cards(card(blue, 0, (_, _)))),
+    assertz(played_cards(card(green, 0, (_, _)))),
+    assertz(played_cards(card(yellow, 0, (_, _)))),
+    assertz(played_cards(card(white, 0, (_, _)))).
+
+% assumes card is valid (correct card to play)
+play_card_on_field(card(Col, Num, (_, _))) :-
+    retractall(played_cards(card(Col, _, (_,_)))),
+    assertz(played_cards(card(Col, Num, (_,_)))).
+
+% 8 information tokens to start
+init_information_token :-
+    retractall(information_token(_)),
+    assertz(information_token(8)).
+
+dec_information_token :-
+    information_token(OldTokens),
+    retractall(information_token(_)),
+    succ(NewTokens, OldTokens),
+    assertz(information_token(NewTokens)).
+
+% 3 chances to miss
+init_miss_chances :-
+    retractall(miss_chances(_)),
+    assertz(miss_chances(3)).
+
+dec_miss_chances :-
+    information_token(OldChances),
+    retractall(information_token(_)),
+    succ(NewChances, OldChances),
+    assertz(information_token(NewChances)).
+
+
+% valid_play is true if card(C, N0) can be played without causing a miss
+valid_play(card(C, N0, _), Played) :-
+    member(card(C, N1, _), Played),
+    N1 is N0-1.
+
+% replace_pair changes the value of the pair with key K to N, if the pair exists
+replace_pair(_, _, [], []).
+replace_pair(K, N, [(K, _)|T], [(K, N)|T]).
+replace_pair(K, N, [(X, Y)|T], [(X, Y)|NewT]) :-
+    dif(K, X),
+    replace_pair(K, N, T, NewT).
+
+% starting_played([(red, 0), (blue, 0), (yellow, 0), (green, 0), (white, 0)]).
+% starting_discard([]).
+
+% TODO
+% deal_cards()
+
+%Draw top card of the deck into player hand on the left
+draw_card(state(player(hand,Name),Team,[H|T],Info)) :- state(player([H|hand],Name),Team,T,Info).
+
+make_card(Col, Num, card(Col, Num, (false, false))).
+
+make_color_cards(_, [], []).
+make_color_cards(Col, [Num|T], [Card|Rest]) :-
+    make_card(Col, Num, Card),
+    make_color_cards(Col, T, Rest).
+
+make_deck([], _, []).
+make_deck([Col|T1], Nums, Return) :-
+    make_color_cards(Col, Nums, ColCards),
+    make_deck(T1, Nums, Rest),
+    append(ColCards, Rest, Return).
+
+init_deck(Deck) :- make_deck([red,blue,green,white,yellow], [1,1,1,2,2,3,3,4,4,5], Deck).
+% append(L1, L2, Combined)
--- main.pl
+++ main.pl
@@ -5,6 +5,15 @@
 %
 %
 
+/*
+card(Col, Num, (ColKnown, NumKnown))
+Col = red ; yellow ; green ; blue ; white .
+
+state(Player,Team,Deck,Info)
+team(Player,Player,Player)
+player(Hand,Name)
+info(Tokens,Miss,Played,Discard,HintLog)
+*/
 
 % start
 % starts the application by loading the knowledge base and starting the main menu
@@ -43,40 +52,93 @@ read_rules :-
     writeln('Detailed rules found on the wikipedia article'),
     writeln('https://en.wikipedia.org/wiki/Hanabi_(card_game)'), nl,
     writeln('Welcome to game of Hanabi!'), nl,
-    writeln('TLDR: Play with 4 friends and work together to play your hands in order'),
+    writeln('TL;DR: Play with 4 friends and work together to play your hands in order'),
     writeln('You each are given a hand of 5 cards, where you cant see your own hand but can see your teammates'),
-    writeln('Cards have a color [red, blue, green, yellow] and a number [1-5]'),
-    writeln('The gamefield starts with a [Red 0], [Green 0], [Yellow 0], and a [Blue 0]'),
+    writeln('Cards have a color [red, blue, green, yellow, or white] and a number [1-5]'),
+    writeln('The gamefield starts with a [Red 0], [Green 0], [Yellow 0], [Blue 0], and a [White 0]'),
     writeln('Your goal as a team is to count each colour to 5 going in order'), nl,
     writeln('During your turn you have 3 options'),
     writeln('[1] Use one of your information tokens to tell your teammates information about their hand'),
     writeln('[2] Play a card onto the field (blind!) and guess that you are making a valid move'),
-    writeln('[3] Discard one of you cards'), nl, nl.
+    writeln('[3] Discard one of your cards to gain an information token'), nl, nl.
 
 start_game :-
-    writeln('Starting up the game make sure you have all 4 people present and number yourselves 1-4'), nl, nl,
-%    deal_cards,
-    gameplay_loop(0).
+    writeln('Starting up the game… make sure you have all 4 people present and number yourselves 1-4'), nl, nl,
+    % TODO:
+        % player name input
+        % game state initialization: deal cards
+    % TEMP
+    gameplay_loop(state(player([], 'a'), team(player([], 'b'), player([], 'c'), player([], 'd')), [], info(8, 0, [], []))).
 
-
-gameplay_loop(Player) :- 
-    confirm_player_ready(Player).
-%    print_info,
+gameplay_loop(state(Player, team(P2, P3, P4), Deck, Info)) :- 
+    confirm_player_ready(Player),
+    print_info(state(Player, Team, Deck, Info)).
 %    poll_for_options.
+    gameplay_loop(state(P2, team(P3, P4, Player), Deck, Info)).
 
-confirm_player_ready(Player) :-
-    format('Hey make sure its only [Player ~d] looking at the screen', [Player]), nl,
-    writeln('type "okay." when you are ready to go!'),
+confirm_player_ready(player(Hand, Name)) :-
+    write('Hey, make sure only '), write(Name), write('is looking at the screen.'), nl,
+    writeln('Type "okay." when you are ready!'),
     catch(
         read(Ans),
         error(syntax_error(_), _),
-        (writeln('Input not applicable. Please re-enter.'), nl, main_menu)
+        (writeln('Invalid input, please re-enter.'), nl, main_menu)
     ), nl,
 
     (Ans = 'okay' -> 
-         writeln('sounds good champ!');
-         writeln('Try again loser'),
+         writeln('Sounds good, champ!'), nl;
+         writeln('Try again!'),
          confirm_player_ready(Player)).
 
-         
+% TODO: add played + discarded cards
+% print_info(state(player([card(green,3,(false,false)),card(blue,2,(true,false)),card(red,3,(false,true)),card(white,1,(true,true))],'baba'),team(player([card(red,2,(false,true)),card(blue,4,(true,false)),card(yellow,4,(true,true)),card(white,1,(true,true))],'keke'),player([card(yellow,2,(false,true)),card(green,3,(true,false)),card(yellow,1,(true,true)),card(green,1,(true,true))],'jiji'),player([card(white,2,(false,true)),card(red,5,(true,false)),card(white,3,(true,true)),card(white,3,(true,true))],'fofo')),[],info(7,1,[],[]))).
+print_info(state(player(H1, P1), team(player(H2, P2), player(H3, P3), player(H4, P4)), Deck, info(Tokens, Miss, Played, Discard))) :-
+    write(P2), write('''s hand: ['), print_hand(H2), write(']'), nl,
+    write(P3), write('''s hand: ['), print_hand(H3), write(']'), nl,
+    write(P4), write('''s hand: ['), print_hand(H4), write(']'), nl,
+    write('Your hand: ['), print_hidden_hand(H1), write(']'), nl, nl,
+    format('~d information tokens, ~d misses~n', [Tokens, Miss]),
+    writeln('Played: TODO'), writeln('Discarded: TODO').
 
+% card(Col, Num, (ColKnown, NumKnown))
+print_hand([]).
+print_hand([card(Col, Num, _)|T]) :-
+    print_card(card(Col, Num, _)),
+    (dif(T, []) ->
+        write(', '), print_hand(T);
+        write('')).
+
+print_hidden_hand([]).
+print_hidden_hand([card(Col, Num, (CK, NK))|T]) :-
+    print_hidden_card(card(Col, Num, (CK, NK))),
+    (dif(T, []) ->
+        write(', '), print_hidden_hand(T);
+        write('')).
+
+print_card(card(Col, Num, _)) :-
+    stringify(Col, S),
+    write(S),
+    format('-~d', [Num]).
+
+print_hidden_card(card(_, _, (false, false))) :-
+    write('???-?').
+print_hidden_card(card(_, Num, (false, true))) :-
+    format('???-~d', [Num]).
+print_hidden_card(card(Col, _, (true, false))) :-
+    stringify(Col, S),
+    write(S),
+    write('-?').
+print_hidden_card(card(Col, Num, (true, true))) :-
+    print_card(card(Col, Num, (true, true))).
+
+stringify(red, 'RED').
+stringify(yellow, 'YELLOW').
+stringify(green, 'GREEN').
+stringify(blue, 'BLUE').
+stringify(white, 'WHITE').
+
+% CARD:  COLOR--NUMBER
+% ???-?
+% ???-5
+% RED-?
+% RED-5
